6c3e250d6ad2e336d6caf8d023a832f169187310
Charbel Choueiri <charbel.choueiri@gmail.com>
2018-06-28 02:45:09 -0400

    Initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # google-ddns
 Google Dynamic DNS update tool.
+
+
+## Features
+* No dependencies, uses only build-in node packages.
---
+++ index.js
@@ -0,0 +1,5 @@
+
+const GoogleDynamicDNS = require('./lib/GoogleDynamicDNS');
+
+let googleDynamicDNS = new GoogleDynamicDNS({hostname: 'trio.deda.ca', username: 'kTLd2Unn0bKx0aab', password: 'rvRFzbjO63QKD7wY'});
+googleDynamicDNS.run();
